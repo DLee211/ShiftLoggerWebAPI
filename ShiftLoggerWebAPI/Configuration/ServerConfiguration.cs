9e1d826ec96b0cc18348870b404080b4c8f26a4b
DLee211 <daniel.lee1921@gmail.com>
2024-02-18 13:09:48 -0500

    Configured it to work with My SQL Server Docker on Mac

--- ShiftLoggerWebAPI/Configuration/ServerConfiguration.cs
+++ ShiftLoggerWebAPI/Configuration/ServerConfiguration.cs
@@ -2,6 +2,13 @@
 
 public class ServerConfiguration
 {
-    public static string DatabaseConnectionString { get; } = "Server=(localdb)\\MSSQLLocalDB;Database=workershifts.db;Integrated Security=True";
+    private static string password = "";
+    
+    public static string
+        DatabaseConnectionString
+    {
+        get;
+    } = //"Server=(localdb)\\MSSQLLocalDB;Database=workershifts.db;Integrated Security=True";//Windows
+        $"Server=localhost,1433;Database=workershifts;User Id=SA;Password={password};TrustServerCertificate=True";//Mac
 
 }
